d0f29b5c628293c576fe1a43b111e465510814db
Rafael Fernandes <rafaelsf013@gmail.com>
2023-06-30 12:53:56 -0300

    feat: Add request

--- src/server.js
+++ src/server.js
@@ -8,6 +8,27 @@ const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 
+app.post('/request', async (req, res, next) => {
+
+    res.header('Access-Controll-Allow-Origin', 'http://localhost:5173')
+    res.header('Referrer-Policy', 'no-referrer-when-downgrade')
+
+    const redirectUrl = 'http://127.0.0.1:3000/oauth'
+
+    const OAuth2Client = new OAuth2Client(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        redirectUrl
+    )
+
+    const authorizeUrl = OAuth2Client({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile openid',
+        prompt: 'consent'
+    })
+
+})
+
 app.listen(PORT, () => {
 
     console.log(`Server running on port ${PORT}`)
