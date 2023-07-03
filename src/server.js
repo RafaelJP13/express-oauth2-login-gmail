f2a4e1f47ad55be81e0c981c01828b39be19b966
Rafael Fernandes <rafaelsf013@gmail.com>
2023-07-03 08:51:35 -0300

    feat: Add oauth method

--- src/server.js
+++ src/server.js
@@ -31,6 +31,32 @@ app.post('/request', async (req, res, next) => {
 
 })
 
+
+app.get('/oauth', async (req, res, next) => {
+
+    const code = req.query.code
+    try{
+ 
+     const redirectUrl = 'http://127.0.0.1:3000/oauth'
+     const oAuth2Client = new OAuth2Client(
+         process.env.CLIENT_ID,
+         process.env.CLIENT_SECRET,
+         redirectUrl
+     )
+ 
+     const res = await oAuth2Client.getToken(code)
+     await oAuth2Client.setCredentials(res.tokens)
+     console.log('Tokens Acquired')
+     const user = oAuth2Client.credentials
+     console.log('Credentials:', user)
+     await getUserData(user.access_token)
+    }catch(err){
+ 
+     console.log(err)
+    }
+ 
+ })
+
 const getUserData = async access_token =>{
 
     const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`)
